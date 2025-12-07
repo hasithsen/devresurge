0384e734ed5341ac2d31bd9ca55ea09b70927d30
hasithsen <sen.hasith@gmail.com>
2025-12-07 11:50:15 +0000

    fix: Update anymail email config for resend
    Signed-off-by: hasithsen <sen.hasith@gmail.com>

--- config/settings/production.py
+++ config/settings/production.py
@@ -101,8 +101,10 @@ INSTALLED_APPS += ["anymail"]
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
 # https://anymail.readthedocs.io/en/stable/esps
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-ANYMAIL = {}
+EMAIL_BACKEND = "anymail.backends.resend.EmailBackend"
+ANYMAIL = {
+    "RESEND_API_KEY": env("RESEND_API_KEY"),
+}
 
 
 # LOGGING
